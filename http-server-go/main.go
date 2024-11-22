9662cfad7f0cad46d1ca2a996135bcf78f5a0395
allantabilog <allan.tabilog@gmail.com>
2024-11-22 15:32:06 +1100

    Implement /user-agent request handler

--- http-server-go/main.go
+++ http-server-go/main.go
@@ -53,7 +53,13 @@ func handleRequest(conn net.Conn) {
 		conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
 		return
 	}
-
+	// extract any headers from the request
+	headers, err := requestParser.parseHeaders(request)
+	if err != nil {
+		fmt.Println("Failed to parse headers")
+		conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+		return
+	}
 	fmt.Printf("verb: %v\n", requestLine.Verb)
 	fmt.Printf("path: %v\n", requestLine.Path)
 	fmt.Printf("version: %v\n", requestLine.Version)
@@ -64,7 +70,7 @@ func handleRequest(conn net.Conn) {
 	case strings.HasPrefix(requestLine.Path, "/echo"):
 		handleEchoRequest(requestLine.Path, conn)
 	case strings.HasPrefix(requestLine.Path, "/user-agent"):
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		handleUserAgentRequest(headers, conn)
 	default:
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
@@ -83,5 +89,16 @@ func handleEchoRequest(path string, conn net.Conn) {
 	// construct the response
 	response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)
 
+	conn.Write([]byte(response))
+}
+
+func handleUserAgentRequest(headers map[string]string, conn net.Conn) {
+	// extract the user agent from the request
+	userAgent, ok := headers["User-Agent"]
+	if !ok {
+		conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+		return
+	}
+	response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)
 	conn.Write([]byte(response))
 }
